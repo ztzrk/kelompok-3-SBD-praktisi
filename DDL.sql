4978db876149f4b24ad9581fa86b8d426e48836c
EdgarWi1 <edgarsilvanus12@gmail.com>
2022-10-04 19:53:49 +0800

    add file DDL and DML with some code inside

---
+++ DDL.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE praktisi;
+
+USE praktisi;
+
+#buat tabel mahasiswa
+
+CREATE TABLE mahasiswa (
+	id_mahasiswa INT(10) AUTO_INCREMENT PRIMARY KEY,
+	full_name VARCHAR(100),
+	address VARCHAR(100) NOT NULL,
+	major VARCHAR(100) NOT NULL
+);
+
+#buat tabel matkul
+
+CREATE TABLE matkul (
+	id_matkul INT(10) AUTO_INCREMENT PRIMARY KEY,
+	nama VARCHAR(100),
+	durasi INT(10) DEFAULT 0
+);  
+
+#buat tabel jadwal
+
+# ntah kenapa ga jalan
+-- CREATE TABLE jadwal (
+-- 	id_jadwal INT(10) AUTO_INCREMENT PRIMARY KEY,
+-- 	tanggal DATE NOT NULL,
+-- 	jam TIME NOT NULL,
+-- 	FOREIGN KEY (id_matkul) REFERENCES matkul(id_matkul),
+-- 	FOREIGN KEY (id_mahasiswa) REFERENCES mahasiswa(id_mahasiswa)
+-- );
---
+++ DML.sql
@@ -0,0 +1,42 @@
+USE praktisi;
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#data khaibar
+
+
+#data aflah
+
+
+#data fadilah
+
+
+#data dewa
+
+
+#data edgar
+INSERT INTO mahasiswa 
+VALUE ('', 'Edgar Silvanus Limba', 'Jl. Parinring I no.9/17 PERUMNAS ANTANG', 'Sistem Informasi');
+
+UPDATE mahasiswa
+SET address = 'Jl. Parinring I no. 9/17 Perumnas Antang'
+WHERE full_name LIKE 'Edgar%';
+
+#data kahfi
+
+
+#data rafiq
+
+
+#data arjuna
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#input data matkul
+INSERT INTO matkul VALUE ('', 'Sistem Basis Data', 120);
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#input jadwal 
