50f1986cfc24ad30e467ccfa4658af0a253b3739
ztzrk <58360320+ztzrk@users.noreply.github.com>
2022-10-09 22:21:41 +0800

    Update DDL Aflah

--- DDL.sql
+++ DDL.sql
@@ -22,10 +22,12 @@ CREATE TABLE matkul (
 #buat tabel jadwal
 
 # ntah kenapa ga jalan
--- CREATE TABLE jadwal (
--- 	id_jadwal INT(10) AUTO_INCREMENT PRIMARY KEY,
--- 	tanggal DATE NOT NULL,
--- 	jam TIME NOT NULL,
--- 	FOREIGN KEY (id_matkul) REFERENCES matkul(id_matkul),
--- 	FOREIGN KEY (id_mahasiswa) REFERENCES mahasiswa(id_mahasiswa)
--- );
+CREATE TABLE jadwal (
+ 	id_jadwal INT(10) AUTO_INCREMENT PRIMARY KEY,
+ 	tanggal DATE NOT NULL,
+ 	jam TIME NOT NULL,
+	id_matkul INT(10) AUTO_INCREMENT PRIMARY KEY,
+	id_mahasiswa INT(10) AUTO_INCREMENT PRIMARY KEY,
+ 	FOREIGN KEY (id_matkul) REFERENCES matkul(id_matkul),
+ 	FOREIGN KEY (id_mahasiswa) REFERENCES mahasiswa(id_mahasiswa)
+);
